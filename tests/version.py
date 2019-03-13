e85045bb3f91ecccb77067669778312c5517ec58
Jan Pazdziora <jpazdziora@redhat.com>
2019-03-13 09:51:07 +0100

    Fedora 28/Python 3.6 does not support capture_output=True.

--- tests/version.py
+++ tests/version.py
@@ -6,7 +6,7 @@ import libssh
 class VersionTest(unittest.TestCase):
 
 	def test_version(self):
-		rpm_q_libssh = subprocess.run(["rpm", "-q", "--qf", "%{version}", "libssh"], capture_output=True)
+		rpm_q_libssh = subprocess.run(["rpm", "-q", "--qf", "%{version}", "libssh"], stdout=subprocess.PIPE)
 		self.assertIsNotNone(rpm_q_libssh)
 		self.assertIsInstance(rpm_q_libssh, subprocess.CompletedProcess)
 		self.assertIsNotNone(rpm_q_libssh.stdout)
