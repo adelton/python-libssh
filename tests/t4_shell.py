1c068a9166913419138170a1b2c4d8badd0f38e9
Jan Pazdziora <jpazdziora@redhat.com>
2019-03-18 13:30:21 +0100

    In Travis CI, we might be getting Last login fast.

--- tests/t4_shell.py
+++ tests/t4_shell.py
@@ -20,10 +20,10 @@ class ShellTest(unittest.TestCase):
 		self.assertIsInstance(channel, libssh.Channel)
 
 		data = channel.read_nonblocking()
-		self.assertEqual(data, b"")
+		self.assertRegex(data, b"^$|^Last login: ")
 		sleep(1)
 		data = channel.read_nonblocking()
-		self.assertRegex(data, b"^Last login: ")
+		self.assertRegex(data, b"^Last login: |\[root@.+ ~\]# $")
 
 		channel.write(b"echo hello world\n")
 		sleep(1)
